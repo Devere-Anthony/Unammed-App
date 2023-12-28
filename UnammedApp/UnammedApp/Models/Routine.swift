f2114b2b4350f564620293016ce6ad7f4be2e52f
Devere-Anthony <devereaweaver@icloud.com>
2023-12-27 21:01:18 -0500

    updated Routine, ProgramTableViewController, and NewRoutineTableViewController

--- UnammedApp/UnammedApp/Models/Routine.swift
+++ UnammedApp/UnammedApp/Models/Routine.swift
@@ -10,4 +10,18 @@ import Foundation
 struct Routine {
     var name: String
     var lifts: [Lift]
+    
+    init() {
+        // empty initializer
+        self.name = ""
+        self.lifts = []
+    }
+    
+    init(name: String, lifts: [Lift]) {
+        // manually initialize name and liftsj
+        self.name = name
+        self.lifts = lifts
+    }
+    
+   
 }
--- UnammedApp/UnammedApp/NewRoutineTableViewController.swift
+++ UnammedApp/UnammedApp/NewRoutineTableViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ProgramTableViewController.swift
+//  NewRoutineTableViewController.swift
 //  UnammedApp
 //
 //  Created by Devere Weaver on 12/27/23.
@@ -7,33 +7,12 @@
 
 import UIKit
 
-class ProgramTableViewController: UITableViewController {
+class NewRoutineTableViewController: UITableViewController {
     
-    // create some dummy Routines so that we can decide if the table is working
-    let dayA = Routine(name: "Day A",
-                       lifts: [
-                        Lift(name: "Low-bar Squat", workingSets: 3, workingReps: 5, targetWeight: 335),
-                        Lift(name: "Overhead Press", workingSets: 3, workingReps: 5, targetWeight: 135),
-                        Lift(name: "Deadlift", workingSets: 1, workingReps: 5, targetWeight: 345)
-                       ])
-    let dayB = Routine(name: "Day B",
-                       lifts: [
-                        Lift(name: "Low-bar Squat", workingSets: 3, workingReps: 5, targetWeight: 335),
-                        Lift(name: "Bench Press", workingSets: 3, workingReps: 5, targetWeight: 225),
-                        Lift(name: "Power-clean", workingSets: 5, workingReps: 3, targetWeight: 135)
-                       ])
-    var routines: [Routine] {
-        // have to use a computed property here because self relies on the routines and the routines rely on self
-        // which creates a circular dependency, just just a computed property that is executed once the object has
-        // be instantiated
-        return [dayA, dayB]
-    }
-    //var routines: [Routine] = []
+    let routine = Routine()    // empty Routine to edit properties
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationItem.leftBarButtonItem = editButtonItem
-
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -41,37 +20,26 @@ class ProgramTableViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-    
-    // MARK: - Table view delegate
-    @IBAction func unwindToRoutines(unwindSegue: UIStoryboardSegue)
-    {
-    }
-    
+
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return routines.count
+        // #warning Incomplete implementation, return the number of rows
+        return 1
     }
 
-   
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Routine", for: indexPath)
-        
-        // update cell properties
-        cell.accessoryType = .disclosureIndicator
-        
-        // configure the cell to display the model object's data
-        let routine = routines[indexPath.row]
-        var content = cell.defaultContentConfiguration()
-        content.text = routine.name
-        cell.contentConfiguration = content
+        let cell = tableView.dequeueReusableCell(withIdentifier: "NewRoutine", for: indexPath)
+//        var content = cell.defaultContentConfiguration()
+//        content.text = "Name"
+//        cell.contentConfiguration = content
         return cell
     }
-  
 
     /*
     // Override to support conditional editing of the table view.
